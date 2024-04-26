f42ad608826349e18eee6b9bc67b2d4812ce87d3
Pablo <jdommay863@g.educaand.es>
2024-04-26 13:57:53 +0200

    añadido modelo

--- codigo/biblioteca/src/main/java/com/endes/biblioteca/BookItem.java
+++ codigo/biblioteca/src/main/java/com/endes/biblioteca/BookItem.java
@@ -5,5 +5,24 @@ public class BookItem extends Book{
 	private String barcode;
 	private RFID tag;
 	private Boolean isRefereceOnly;
+	
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public RFID getTag() {
+		return tag;
+	}
+	public void setTag(RFID tag) {
+		this.tag = tag;
+	}
+	public Boolean getIsRefereceOnly() {
+		return isRefereceOnly;
+	}
+	public void setIsRefereceOnly(Boolean isRefereceOnly) {
+		this.isRefereceOnly = isRefereceOnly;
+	}
 
 }
